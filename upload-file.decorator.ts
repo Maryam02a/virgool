dae3c7779b3f7883630f11bc1d71c7a607e7fcb0
Maryam02a <mrymbadrr@gmail.com>
2026-01-07 22:52:11 +0330

    Delete upload-file.decorator.ts

--- upload-file.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { ParseFilePipe, UploadedFiles } from "@nestjs/common";
-
-export function UploadedOptionalFiles(){
-    return UploadedFiles(new ParseFilePipe({
-        fileIsRequired:false,
-        validators: []
-    }))
-}
